0e66f7f4ebcfeae88357303f572d86f7070ee22f
Marcy_ <marciagisseli21@gmail.com>
2025-11-15 18:09:25 -0300

    :sparkles: colocando icones nas categorias e fazendo o efeito de hover e aumentando a imagem

--- src/assets/pages/home/Home.tsx
+++ src/assets/pages/home/Home.tsx
@@ -1,10 +1,47 @@
-
+import {BabyIcon, HighlighterCircleIcon, PillIcon, ShowerIcon, StethoscopeIcon} from "@phosphor-icons/react"
 
 function Home() {
     return (
         <>
-        <div>Home</div>
+        {/* carrossel */}
+        <div
+            className="bg-gray-400"
+        >Home</div>
 
+        {/* categoria */}
+            <div
+                className="flex justify-center items-center gap-30 h-23"
+            >
+                <div
+                
+                ><HighlighterCircleIcon size={32} weight="regular"
+                    className="hover:scale-110 transition-transform duration-300 ease-in-out"
+                />
+                </div>
+
+                <div>
+                    <ShowerIcon size={32} weight="regular"
+                        className="hover:scale-110 transition-transform duration-300 ease-in-out"
+                    />
+                </div>
+
+                <div>
+                    <PillIcon size={32} weight="regular"
+                        className="hover:scale-110 transition-transform duration-300 ease-in-out"
+                    />
+                </div>
+                <div>
+                    <StethoscopeIcon size={32} 
+                        className="hover:scale-110 transition-transform duration-300 ease-in-out"
+                    />
+                </div>
+                <div>
+                    <BabyIcon size={32} weight='regular'
+                        className="hover:scale-110 transition-transform duration-300 ease-in-out"
+                    />
+                </div>
+            </div>
+        {/* cards-promoção */}
         </>
     )
 }
