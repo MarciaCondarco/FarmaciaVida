a6bd775a86435633042fa14756267a3ab563f461
Marcy_ <marciagisseli21@gmail.com>
2025-11-16 21:43:09 -0300

    :sparkles:  aumentando o numero de cards

--- src/components/promocao/listapromocao/ListaPromocao.tsx
+++ src/components/promocao/listapromocao/ListaPromocao.tsx
@@ -8,7 +8,13 @@ function ListaPromocao() {
     return (
         <>
             <div className="grid grid-cols-4">    
-                
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
                 <Card/>
             </div>
         </>
