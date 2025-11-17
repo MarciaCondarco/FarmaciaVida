cc0eacf69d0e7f6f9875d58e094479a5e976b3f6
Marcy_ <marciagisseli21@gmail.com>
2025-11-16 21:41:25 -0300

    :sparkles: comentando

--- src/components/promocao/listapromocao/ListaPromocao.tsx
+++ src/components/promocao/listapromocao/ListaPromocao.tsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import type Produto from "../../../models/Produto";
+// import { useState } from "react";
+// import type Produto from "../../../models/Produto";
 import Card from "../cardpromocao/Card";
 
 function ListaPromocao() {
 
-    const [Produto] = useState<Produto[]>([])
+    // const [Produto] = useState<Produto[]>([])
     return (
         <>
             <div className="grid grid-cols-4">    
                 
-                <Card  key={Produto.id}/>
+                <Card/>
             </div>
         </>
     )
